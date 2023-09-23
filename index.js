e353c2b2b181a2212ef026ffe96a075aae8d777d
lorenapinkala <baeksumin77@gmail.com>
2023-09-22 21:12:19 -0300

    cors updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: 'https://nextflix-cyan-nu.vercel.app/'
 }));
  app.use(express.json());
  app.use(volleyball);
